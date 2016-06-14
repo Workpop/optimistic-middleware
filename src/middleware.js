2acede4ab5ee1fb4c2761c67a36222e8d78e7a47
abhiaiyer91 <abhiaiyer91@gmail.com>
2016-06-14 13:45:33 -0700

    Optimistic Middleware

---
+++ src/middleware.js
@@ -0,0 +1,42 @@
+const OPTIMISTIC = {
+  START: "OPTIMISTIC_UPDATE_START",
+  ERROR: "OPTIMISTIC_UPDATE_FAILURE",
+  SUCCESS: "OPTIMISTIC_UPDATE_SUCCESS"
+};
+
+export default function optimisticMiddleware(store) {
+  return (next) => {
+    return (action) => {
+      const storeState = store.getState();
+      const { mutation, stateKey, ...rest } = action;
+      // get the previous state before we start
+      const previousState = storeState && storeState[stateKey] && storeState[stateKey].data;
+      // if we don't have a mutation, proceed like normal
+      if (!mutation) {
+        return next(action);
+      }
+      // apply the optimistic update first. This is described in our reducer for this actionType
+      next({
+        ...rest,
+        optimisticState: OPTIMISTIC.START
+      });
+      // next we're going to call our mutation, because we're in a Meteor context, we are expecting a callback with e,r
+      return mutation((error) => {
+        // if there is an error we need to revert our state back to the initial state before middleware ran
+        if (error) {
+          return next({
+            error: error.reason,
+            data: previousState,
+            optimisticState: OPTIMISTIC.ERROR,
+            type: action.type
+          });
+        }
+        // apply our update again but this time, change the OPTIMISTIC state
+        return next({
+          ...rest,
+          optimisticState: OPTIMISTIC.SUCCESS
+        });
+      });
+    }
+  };
+}
---
+++ test/middleware.spec.js
@@ -0,0 +1,58 @@
+import { expect } from 'chai';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import optimisticMiddleware from '../src/middleware';
+const partial = require('lodash.partial');
+
+function optimisticUpdate(store) {
+  return store.dispatch({
+    type: "UPDATE",
+    stateKey: 'test',
+    mutation: function (cb) {
+      return cb();
+    },
+    data: "FOO"
+  });
+}
+
+function optimisticFail(store) {
+  return store.dispatch({
+    type: "UPDATE",
+    stateKey: 'test',
+    mutation: function (cb) {
+      const partialCb = partial(cb, {reason: "you suck"});
+      return partialCb();
+    },
+    data: "FOO"
+  });
+}
+
+function testReducer(state = { data: "BAR" }, action = {}) {
+  const { data, type, ...rest } = action;
+  if (type === 'UPDATE') {
+    return {
+      data,
+      ...rest
+    };
+  }
+  return state;
+}
+
+const rootReducer = combineReducers({
+  test: testReducer
+});
+
+
+describe('Optimistic Middleware', function () {
+  it('should apply the update with an Optimistc flag', function () {
+    const store = createStore(rootReducer, {}, applyMiddleware(optimisticMiddleware));
+    optimisticUpdate(store);
+    expect(store.getState().test.optimisticState).to.eql("OPTIMISTIC_UPDATE_SUCCESS");
+    expect(store.getState().test.data).to.eql("FOO");
+  });
+  it('should should not update state and have a failed optimistic state', function () {
+    const store = createStore(rootReducer, {}, applyMiddleware(optimisticMiddleware));
+    optimisticFail(store);
+    expect(store.getState().test.optimisticState).to.eql("OPTIMISTIC_UPDATE_FAILURE");
+    expect(store.getState().test.data).to.eql("BAR");
+  });
+});

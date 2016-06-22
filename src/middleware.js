6960f17f27d984bdd47623102bd56cd3d791471f
abhiaiyer91 <abhiaiyer91@gmail.com>
2016-06-22 12:54:56 -0700

    tests and bump version

--- src/middleware.js
+++ src/middleware.js
@@ -3,10 +3,14 @@ const isFunction = require('lodash.isfunction');
 export default function optimisticMiddleware(store) {
   return (next) => {
     return (action) => {
-      const { simulate, onSuccess, onError, async, stateKey, type, data, optimistic, ...rest } = action;
+      const { simulate, onSuccess, onError, async, stateKey } = action;
       // if we don't have a mutation, proceed like normal
-      if (!optimistic) {
+      if (!simulate) {
         return next(action);
+      }
+
+      if (!stateKey) {
+        throw new Error('Must include a stateKey to process actions via Optimistic Middleware');
       }
       // get state of the world before mutation
       const storeState = store.getState();
@@ -16,14 +20,10 @@ export default function optimisticMiddleware(store) {
       // apply the optimistic update first. This is described in our reducer for this actionType
       // if there is a simulation function, run the simulation with dispatch and data from the action bound to it.
       if (isFunction(simulate)) {
-        simulate(next, data);
+        simulate(next, previousState);
       } else {
         // otherwise dispatch the type with the data passed to the action creator
-        next({
-          type,
-          data,
-          ...rest,
-        });
+        next(simulate);
       }
       // next we're going to call our mutation, because we're in a Meteor context, we are expecting a callback with e,r
       return async((error, result) => {
@@ -32,12 +32,15 @@ export default function optimisticMiddleware(store) {
           // if the user supplies an onError callback as a function, call their function with
           // dispatch, the previousState, and the error we received
           if (isFunction(onError)) {
-            return onError(next, previousState, error);
+            return onError(next, error, previousState);
+          }
+          if (isFunction(simulate)) {
+            throw new Error('You passed a simulation function without an onError callback. Simulation functions must use onError callbacks for reverting state');
           }
           // if not, automatically pass the original type action the previousState as data
           return next({
             data: previousState,
-            type,
+            type: simulate.type,
           });
         }
         // apply our update again but this time, change the OPTIMISTIC state
